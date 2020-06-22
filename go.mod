20eff35d1dd0ba11adfc09387d47673b90cf355c
Pedro Capdevila <pcapdevila@matriz.com.ar>
2020-06-22 18:31:52 -0300

    Errores http

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/SantaPesca/baselib
 go 1.14
 
 require (
-	github.com/SantaPesca/users v0.0.0-20200619174833-73598d39d73a
+	github.com/SantaPesca/users v0.0.0-20200619174833-73598d39d73a // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/google/uuid v1.1.1
---
+++ models/error.go
@@ -0,0 +1,19 @@
+package models
+
+type HTTPError string
+
+const (
+	SignupBadRequest           HTTPError = "SIGNUP_BAD_REQUEST"
+	LoginUnauthorized          HTTPError = "LOGIN_UNAUTHORIZED"
+	Unauthorized               HTTPError = "UNAUTHORIZED"
+	BadRequest                 HTTPError = "BAD_REQUEST"
+	InternalServerError        HTTPError = "INTERNAL_SERVER_ERROR"
+	ShopForbidden              HTTPError = "SHOP_FORBBIDEN"
+	TemporalFolderNotFound     HTTPError = "TEMPORALFOLDER_NOT_FOUND"
+	TemporalPhotoNotFound      HTTPError = "TEMPORALPHOTO_NOT_FOUND"
+	RecoveryPasswordBadRequest HTTPError = "RECOVERY_PASSWORD_BAD_REQUEST"
+)
+
+type Error struct {
+	Message HTTPError `json:"message"`
+}
