3263b8f712f3e2378cd3a11a6e174ee03a30b5c8
Martín <mdestefanis@matriz.com.ar>
2020-12-02 18:23:22 -0300

    update reporedis

--- pkg/repository/repo_redis.go
+++ pkg/repository/repo_redis.go
@@ -1,11 +1,12 @@
 package repository
 
 import (
+	"context"
 	"github.com/go-redis/redis/v8"
 )
 
 type RedisRepository struct{}
 
 func (u RedisRepository) CheckIfTokenExists(rdb *redis.Client, token string) error {
-	return rdb.Get(token).Err()
+	return rdb.Get(context.Background(), token).Err()
 }
